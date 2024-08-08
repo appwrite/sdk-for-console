e54981233ecb938f7c1db4a88aa64626691f82a8
root <christyjacob4@gmail.com>
2024-08-08 09:00:03 +0000

    chore: updates for appwrite 1.5.9

--- src/client.ts
+++ src/client.ts
@@ -304,7 +304,7 @@ class Client {
         'x-sdk-name': 'Console',
         'x-sdk-platform': 'console',
         'x-sdk-language': 'web',
-        'x-sdk-version': '0.6.3',
+        'x-sdk-version': '0.6.4',
         'X-Appwrite-Response-Format': '1.5.0',
     };
 
