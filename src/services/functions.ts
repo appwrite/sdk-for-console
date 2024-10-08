91fafec42c80a5c26c4fee04db06a3340924386e
root <matejbaco2000@gmail.com>
2024-10-08 16:04:58 +0000

    Fix execution body type

--- src/services/functions.ts
+++ src/services/functions.ts
@@ -815,7 +815,7 @@ Use the &quot;command&quot; param to set the entrypoint used to execute your cod
      * Trigger a function execution. The returned object will return you the current execution status. You can ping the `Get Execution` endpoint to get updates on the current execution status. Once this endpoint is called, your function execution process will start asynchronously.
      *
      * @param {string} functionId
-     * @param {payload} body
+     * @param {string} body
      * @param {boolean} async
      * @param {string} xpath
      * @param {ExecutionMethod} method
@@ -824,7 +824,7 @@ Use the &quot;command&quot; param to set the entrypoint used to execute your cod
      * @throws {AppwriteException}
      * @returns {Promise<Models.Execution>}
      */
-    async createExecution(functionId: string, body?: payload, async?: boolean, xpath?: string, method?: ExecutionMethod, headers?: object, scheduledAt?: string, onProgress = (progress: UploadProgress) => {}): Promise<Models.Execution> {
+    async createExecution(functionId: string, body?: string, async?: boolean, xpath?: string, method?: ExecutionMethod, headers?: object, scheduledAt?: string): Promise<Models.Execution> {
         if (typeof functionId === 'undefined') {
             throw new AppwriteException('Missing required parameter: "functionId"');
         }
@@ -851,16 +851,15 @@ Use the &quot;command&quot; param to set the entrypoint used to execute your cod
         const uri = new URL(this.client.config.endpoint + apiPath);
 
         const apiHeaders: { [header: string]: string } = {
-            'content-type': 'multipart/form-data',
+            'content-type': 'application/json',
         }
 
 
-        return await this.client.chunkedUpload(
+        return await this.client.call(
             'post',
             uri,
             apiHeaders,
-            payload,
-            onProgress
+            payload
         );
     }
     /**
