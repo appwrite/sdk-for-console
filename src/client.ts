d31574ad250cb0a9510da4190c88b1d30bfd54d2
loks0n <22452787+loks0n@users.noreply.github.com>
2025-01-10 12:41:40 +0000

    chore: fix version

--- src/client.ts
+++ src/client.ts
@@ -316,7 +316,7 @@ class Client {
         'x-sdk-name': 'Console',
         'x-sdk-platform': 'console',
         'x-sdk-language': 'web',
-        'x-sdk-version': '1.2.1',
+        'x-sdk-version': '1.4.6',
         'X-Appwrite-Response-Format': '1.6.0',
     };
 
